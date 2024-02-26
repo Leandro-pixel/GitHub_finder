29aefee4a4c3be9b2093adac93a15f59c2c59344
Leandrovisk <leandro20002004@hotmail.com>
2024-02-26 00:46:05 -0300

    deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 //pages
 import Home from './routes/Home.tsx'
 import Repos from './routes/Repos.tsx'
 
-const router = createBrowserRouter([ //configurando o router
+const router = createHashRouter([ //configurando o router
   {
     path: "/",//primeiro coloca o componente principal que vai abarcar todas as outras
     element: <App/>,
